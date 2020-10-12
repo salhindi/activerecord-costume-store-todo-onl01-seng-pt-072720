7f795dcb9aa19e4053f93041d72e1049f04db9f0
Sara A <calisaraalhindi@gmail.com>
2020-10-12 23:10:00 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,11 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_house do |h|
+      h.string :name 
+      h.string :location
+      h.string :theme
+      h.float :price
+    end
+  end
+end
