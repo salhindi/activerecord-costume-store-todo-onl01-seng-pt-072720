137b13436cd97bf8ad31eaad16f18a9ffaf53471
Sara A <calisaraalhindi@gmail.com>
2020-10-13 16:42:46 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :haunted_houses do |h|
+    create_table :haunted_house do |h|
       h.string :name 
       h.string :location
       h.string :theme
